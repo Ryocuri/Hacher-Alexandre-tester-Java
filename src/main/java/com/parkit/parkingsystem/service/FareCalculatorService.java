2973dd83aae14dd5ff26443a104dcf47fa584519
Alexandre <alexandre.hacher@etu.univ-lehavre.fr>
2023-10-22 22:34:33 +0200

    Changement du système de calcul d'heures (getTime), ajout du parking gratuit en dessous de 30min, ajout des tests

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -10,11 +10,18 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+        double inHour = ticket.getInTime().getTime();
+        double outHour = ticket.getOutTime().getTime();
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+        double duration = outHour - inHour;
+
+        duration = duration / (60 * 60 * 1000);
+
+        if(duration <= 0.5){
+            ticket.setPrice(0.0);
+            return;
+        }
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
